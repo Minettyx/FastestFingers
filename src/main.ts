fc59ed5a53bbb05362c48c36cd3dab48e8993342
Minettyx <30463302+Minettyx@users.noreply.github.com>
2021-06-22 17:58:07 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,33 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+/* Bootstrap */
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+/* my plugins */
+import functions from './plugins/functions'
+import viewport from './plugins/viewport'
+import darkmode from './plugins/darkmode'
+import socket from './plugins/socket'
+
+/* google auth */
+import GAuth from 'vue3-google-oauth2'
+
+const app = createApp(App)
+app.use(router)
+
+app.use(functions)
+app.use(viewport)
+app.use(darkmode)
+app.use(socket)
+
+const gAuthOptions = {
+  clientId: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+  scope: 'openid',
+  prompt: 'consent',
+  fetch_basic_profile: false
+}
+app.use(GAuth, gAuthOptions)
+
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div>
+  <p v-for="msg in messages" :key="msg">{{ msg.username }}: {{ msg.message }}</p>
+
+  <form @submit.prevent="this.$socket.socket.send(search)&&sendMsg()">
+    <input v-model="search" type="search" class="form-control" placeholder="Send">
+  </form>
+
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      messages: [] as {username: string, message: string}[],
+      search: ''
+    }
+  },
+  mounted () {
+    this.$socket.socket.on('message', (data) => {
+      this.messages.push(data)
+    })
+  },
+  methods: {
+    sendMsg () {
+      this.search = ''
+    }
   }
 })
 </script>
